142a9a4363788b7a887862445bb64f7ebba1fdaa
RONANGRELE <55504796+RONANGRELE@users.noreply.github.com>
2019-10-02 09:22:57 -0700

    Add files via upload

---
+++ stringProcessor2.java
@@ -0,0 +1,16 @@
+public class stringProcessor2
+{
+    public static String withoutEnds(String str)
+    {
+        return str.substring(1, str.length() - 1);
+    }
+//    public static boolean frontAgain(String str)
+//    {
+//        if (str.toLowerCase(str.substring(0, 1)).equals(str.toLowerCase(str.substring(str.length() - 2, str.length() - 1)))) 
+//        {
+//        return true;
+//        } else {
+//        return false;
+//        }
+//  }
+    }
