1da92fb47e7805edfdf7d11386934a600dce97e6
RONANGRELE <55504796+RONANGRELE@users.noreply.github.com>
2019-10-04 09:31:55 -0700

    Add files via upload

---
+++ loopsExamples.java
@@ -0,0 +1,27 @@
+public class loopsExamples
+{
+    public static void main(String[] args)
+    {
+ //       int x = 5;
+ //       int y;
+ //       int numCoins = 4;
+ //       numCoins = numCoins + 1;
+ //       numCoins += 1;
+ //       System.out.println("Number of Coins:" + numCoins);
+ //       int numLives = 0;
+ //       numLives = numLives + 3;
+ //       numLives += 3;
+ //       
+ //       numLives = numLives * 3;
+ //       numLives *= 3;
+ //       System.out.println("Number of Lives:" + numLives);
+ //       
+ //       numLives /= 2;
+ //       System.out.println("Number of Lives:" + numLives);
+ //       for(int i = 0; i < 8; i += 1) {
+ //       System.out.println(i);
+        for(int x = 0; x <= 52; x += 2) {
+        System.out.println(x);
+    }
+        }
+    }
